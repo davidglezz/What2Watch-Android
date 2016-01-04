e5ad47bd1ce8746cab16207eb8da5b0884836bdc
davidglezz <davidglezz@outlook.es>
2016-01-04 11:55:51 +0100

    Reordenado + descarga asincrona de poster

--- TestIGUAndroid/MyApplication/app/src/main/java/com/example/user/testiguandroid/Logica/Pelicula.java
+++ TestIGUAndroid/MyApplication/app/src/main/java/com/example/user/testiguandroid/Logica/Pelicula.java
@@ -1,13 +1,21 @@
 package com.example.user.testiguandroid.Logica;
 
 
-public class Pelicula {
-    private String Title;
-    private int Year;
-    private String imdbID; //TODO redefinir equals con esto
-    private String Type;
-    private String poster;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
 
+import java.io.InputStream;
+
+public class Pelicula {
+    private String title;
+    private int year;
+    private String imdbID; //TODO redefinir equals con esto
+    private String type;
+    private String poster;
+    private Bitmap posterBMP;
     private String rated;
     private String released;
     private String runtime;
@@ -15,14 +23,52 @@ public class Pelicula {
     private String director;
     private String writer;
     private String[] actors;
+    private String plot;
+    private String country;
+    private String awards;
+    private String imdbRating;
+    private boolean vista = false;
+    private int nota;
 
-    private boolean vista=false;
-    private  int nota;
-    public void setVista(boolean b){
-        this.vista=b;
+
+    public Pelicula(String title, int year, String imdbID, String type,
+                    String poster, String rated, String released, String runtime,
+                    String genre, String director, String writer, String actors,
+                    String plot, String country, String awards, String imdbRating) {
+        super();
+        this.title = title;
+        this.year = year;
+        this.imdbID = imdbID;
+        this.type = type;
+        this.poster = poster;
+        this.rated = rated;
+        this.released = released;
+        this.runtime = runtime;
+        this.genre = genre;
+        this.director = director;
+        this.writer = writer;
+        this.actors = actors.split(", ");
+        this.plot = plot;
+        this.country = country;
+        this.awards = awards;
+        this.imdbRating = imdbRating;
     }
-    private void setNota(int n){
-        this.nota=n;
+
+    public Pelicula(String title, int year, String imdbID, String type, String poster) {
+        super();
+        this.title = title;
+        this.year = year;
+        this.imdbID = imdbID;
+        this.type = type;
+        this.poster = poster;
+    }
+
+    public void setVista(boolean b) {
+        this.vista = b;
+    }
+
+    private void setNota(int n) {
+        this.nota = n;
     }
 
     public String getRated() {
@@ -57,63 +103,18 @@ public class Pelicula {
         return plot;
     }
 
-    public String getCountry() {
-        return country;
-    }
+    public String getCountry() { return country;  }
 
-    public String getAwards() {
-        return awards;
-    }
+    public String getAwards() { return awards; }
 
-    public String getImdbRating() {
-        return imdbRating;
-    }
-
-    private String plot;
-    private String country;
-    private String awards;
-    private String imdbRating;
-
-
-    public Pelicula(String title, int year, String imdbID, String type,
-                    String poster, String rated, String released, String runtime,
-                    String genre, String director, String writer, String actors,
-                    String plot, String country, String awards, String imdbRating) {
-        super();
-        Title = title;
-        Year = year;
-        this.imdbID = imdbID;
-        Type = type;
-        this.poster = poster;
-        this.rated = rated;
-        this.released = released;
-        this.runtime = runtime;
-        this.genre = genre;
-        this.director = director;
-        this.writer = writer;
-        this.actors = actors.split(", ");
-        this.plot = plot;
-        this.country = country;
-        this.awards = awards;
-        this.imdbRating = imdbRating;
-    }
-
-    public Pelicula(String title, int year, String imdbID, String type,
-                    String poster) {
-        super();
-        Title = title;
-        Year = year;
-        this.imdbID = imdbID;
-        Type = type;
-        this.poster = poster;
-    }
+    public String getImdbRating() { return imdbRating;  }
 
     public String getTitle() {
-        return Title;
+        return title;
     }
 
     public int getYear() {
-        return Year;
+        return year;
     }
 
     public String getImdbID() {
@@ -121,7 +122,7 @@ public class Pelicula {
     }
 
     public String getType() {
-        return Type;
+        return type;
     }
 
     public String getPoster() {
@@ -129,6 +130,38 @@ public class Pelicula {
     }
 
 
+    public void setPosterToView(ImageView imgPoster) {
+        if (posterBMP == null) {
+            new DownloadImageTask(imgPoster).execute(getPoster());
+        } else {
+            imgPoster.setImageBitmap(posterBMP);
+        }
+    }
 
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
 
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                Log.v("-------", "Descargando");
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            posterBMP = result;
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
